1b76549379e82c694b2acca2a0e61a448dd0851d
Mauro Vogel <mavogel@student.ethz.ch>
2023-09-19 08:34:22 +0200

    test commit webstorm

--- modules/Cytoscape.js
+++ modules/Cytoscape.js
@@ -38,3 +38,34 @@ var cy = cytoscape({
     position: { x: 200, y: 200 },
   });
 
+function cyCreateNode(id, xPos=200, yPos=200, color='grey', border=false){
+    //ensure creating within box & distance from each other
+    let xDistance = 150;
+    let yDistance = 100;
+    xPos = Math.max(50, (50 + xDistance*id) % document.getElementById('cytoscape').clientWidth);
+    yPos = 80 + yDistance*(Math.floor((50 + id*150) / document.getElementById('cytoscape').clientWidth));
+    let label = id;
+    //border
+    let borderWidth = 0;
+    let borderColor = 'white';
+    if(border){
+        borderWidth = 2;
+        borderColor = 'black'
+    }
+
+    //core
+    cy.add({
+        group: 'nodes',
+        data: {id: id},
+        style: {
+            'background-color': `${color}`,
+            'border-width': `${borderWidth}`, // Set the border width for the nodes
+            'border-color': `${borderColor}`,
+            'label': `${label}`,
+            "text-valign": "center",
+            "text-halign": "center",
+        },
+        position: { x: xPos, y: yPos},
+    });
+}
+
