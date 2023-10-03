f7f43477b3a7aeaf1ed7c28de0b0b05ccd504340
Mauro Vogel <mavogel@student.ethz.ch>
2023-10-03 14:03:18 +0200

    edit & delete nodes

--- modules/Cytoscape.js
+++ modules/Cytoscape.js
@@ -8,6 +8,9 @@ export {cy, cyCreateNode, cyCreateEdge, nodePresetHelper, nodePresetReset, cyCle
 var nodeId = 0;
 //fromNode at Edge Creation (used to safe on which node the user clicked)
 var fromNode;
+//editNode to save node clicked on to edit
+var cytoEditNode;
+var editNode;
 
 
 //////////////////////////////////////////////////////////////
@@ -140,7 +143,7 @@ function cyClearCanvas(){
 
 
 //////////////////////////////////////////////////////////////
-//// -------------------- User Input -------------------- ////
+//// ------------------ User Creation ------------------- ////
 //////////////////////////////////////////////////////////////
 //// ----------- Node Creation
 //dblclick -> create node
@@ -157,16 +160,30 @@ function cyClearCanvas(){
     const leftValue = parseInt(window.getComputedStyle(cytoWindow).getPropertyValue('left'), 10);
     const topValue = parseInt(window.getComputedStyle(cytoWindow).getPropertyValue('top'), 10);
 
+    ////change button from "edit node" to "create node"
+    var nodeEditButton = document.getElementById("nodeEditButton");
+    if(nodeEditButton){
+        var nodeButton = document.createElement("button");
+        nodeButton.id = "nodeButton";
+        nodeButton.innerText = "Create Node";
+        // Replace the existing button with the new button
+        nodeEditButton.parentNode.replaceChild(nodeButton, nodeEditButton);
+    }
+
+
+
+
     //display modal at doubleclick position
     modal.style.marginLeft = `${position.x + leftValue}px`
     modal.style.marginTop = `${position.y + topValue}px`;
     nodeModal.style.display = 'block';
 })
 
-//user submit node inputs
+//user submit node inputs (Event Listener)
 document.getElementById('nodeButton').addEventListener('click', function(){
     userNodeInputHandler();
 })
+
 function userNodeInputHandler(){
     //Close the modal
     nodeModal.style.display = 'none';
@@ -212,6 +229,12 @@ function userNodeInputHandler(){
 }
 //Cancel button (node) pressed
 document.getElementById("cancelButton").addEventListener('click', function(){
+    //remove delete node button
+    const deleteButton = document.getElementById("nodeDeleteButton");
+    if(deleteButton){
+        document.getElementById("deleteNodeDiv").removeChild(deleteButton);
+    }
+
     nodeModal.style.display = 'none';
 })
 
@@ -320,7 +343,6 @@ document.getElementById("cancelButton2").addEventListener('click', function(){
     edgeModal.style.display = 'none';
 })
 
-
 //Helper: Creates dropdown menus dynamically
 function createDropdownMenues(){
     //// toNode
@@ -345,6 +367,169 @@ function createDropdownMenues(){
 
     ////
 }
+
+
+//////////////////////////////////////////////////////////////
+//// -------------------- User Edit --------------------- ////
+//////////////////////////////////////////////////////////////
+cy.on('cxttap', 'node', function(event){
+    event.preventDefault();
+    console.log("right click on node")
+
+    //save node clicked on (global vars)
+    cytoEditNode = event.target;
+    editNode = turingMachine.getStatebyId(cytoEditNode.id());
+
+    //open Modal
+    //get click position
+    const position = event.position;
+
+    //
+    const nodeModal = document.getElementById('nodeModal');
+    const modal = document.querySelector('.modal-content');
+
+    //get cytoscape window position
+    const cytoWindow = document.querySelector('#cytoscape');
+    const leftValue = parseInt(window.getComputedStyle(cytoWindow).getPropertyValue('left'), 10);
+    const topValue = parseInt(window.getComputedStyle(cytoWindow).getPropertyValue('top'), 10);
+
+
+    ////change button from "create node" to "edit node"
+    var nodeButton = document.getElementById("nodeButton");
+    if(nodeButton){
+        var nodeEditButton = document.createElement("button");
+        nodeEditButton.id = "nodeEditButton";
+        nodeEditButton.innerText = "Edit Node";
+        // Replace the existing button with the new button
+        nodeButton.parentNode.replaceChild(nodeEditButton, nodeButton);
+    }
+
+    ////Create Delete button (if not yet existing)
+    const deleteButton = document.getElementById("nodeDeleteButton");
+    if(!deleteButton){
+        var newButton = document.createElement("button");
+        newButton.id = "nodeDeleteButton";
+        newButton.innerText = "Delete Node";
+        document.getElementById("deleteNodeDiv").appendChild(newButton);
+
+            //user delete node (Event Listener)
+        newButton.addEventListener('click', function(){
+        userDeleteNodeHandler();
+        })
+    }
+    else{
+        //user delete node (Event Listener)
+        deleteButton.addEventListener('click', function(){
+        userDeleteNodeHandler();
+        })
+    }
+
+
+
+
+
+
+    ////get current node properties
+    //get name
+    document.getElementById("stateName").value = editNode.name;
+    //get Starting/Accepting/Rejecting property
+    document.getElementById("stateStarting").checked = false;
+    document.getElementById("stateAccepting").checked = false;
+    document.getElementById("stateRejecting").checked = false;
+    if(editNode.isStarting){
+        document.getElementById("stateStarting").checked = true;
+    }
+    if(editNode.isAccepting){
+        document.getElementById("stateAccepting").checked = true;
+    }
+    if(editNode.isRejecting){
+        document.getElementById("stateRejecting").checked= true;
+    }
+
+
+    //display modal at node position
+    modal.style.marginLeft = `${position.x + leftValue}px`
+    modal.style.marginTop = `${position.y + topValue}px`;
+    nodeModal.style.display = 'block';
+
+
+    //user submit node inputs (Event Listener)
+    document.getElementById('nodeEditButton').addEventListener('click', function(){
+        userEditNodeHandler();
+    })
+
+})
+
+function userEditNodeHandler(){
+    //close modal
+    nodeModal.style.display = 'none';
+    //remove delete node button
+    let deleteButton = document.getElementById("nodeDeleteButton");
+    if(deleteButton){
+        document.getElementById("deleteNodeDiv").removeChild(deleteButton);
+    }
+    
+
+    ////Name
+    var newName = document.getElementById("stateName").value;
+    //cyto
+    cytoEditNode.style('label', newName);
+    runLayout();
+    //TM object (node)
+    editNode.name = newName;
+
+    ////Starting/Accepting/Rejecting property
+    var isStarting = document.getElementById("stateStarting").checked;
+    if (isStarting){
+        //cyto
+        cytoEditNode.style('background-color', 'darkgrey');
+        cytoEditNode.style('border-width', 2);
+        cytoEditNode.style('border-color', "black");
+        //TM object
+        turingMachine.startstate = editNode;
+        editNode.isStarting = true;
+    }
+
+    var isAccepting = document.getElementById("stateAccepting").checked;
+    if (isAccepting){
+        //cyto
+        cytoEditNode.style('background-color', 'limegreen');
+        //TM object
+        turingMachine.acceptstate = editNode;
+        editNode.isAccepting = true;
+    }
+
+    var isRejecting = document.getElementById("stateRejecting").checked;
+    if (isRejecting){
+        //cyto
+        cytoEditNode.style('background-color', 'red');
+        //TM object
+        turingMachine.rejectstate = editNode;
+        editNode.isRejecting = true;
+    }
+}
+
+function userDeleteNodeHandler(){
+    //close modal
+    nodeModal.style.display = 'none';
+
+    ////Delete in cyto
+    cy.remove(cytoEditNode);
+
+    ////delete from TM object
+    //remove node
+    turingMachine.states.delete(editNode);
+    //remove all edges from / to this node
+    let updatedDelta = new Map();
+    for(const [key, value] of turingMachine.delta){
+        if(key[0] !== editNode && value[0] !== editNode){
+            updatedDelta.set(key, value)
+        }
+    }
+    turingMachine.delta = updatedDelta;
+
+}
+
 
 
 
