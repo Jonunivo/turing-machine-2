510d659d611ca9246539e907576731f0f30b6f0d
Mauro Vogel <mavogel@student.ethz.ch>
2023-11-08 12:49:17 +0100

    ready for ABTest

--- modules/Cytoscape.js
+++ modules/Cytoscape.js
@@ -31,7 +31,9 @@ var cy = cytoscape({
             'curve-style': 'bezier',
             'control-point-distance': 50,
             'control-point-weight': 0.5, 
-            'loop-direction': '0deg'
+            'loop-direction': '0deg',
+            'text-margin-x': "-10px",
+            'text-margin-y': "-5px",
         }
         
     }],
@@ -117,8 +119,6 @@ function cyCreateEdge(fromNode, toNode, label, readToken){
         style: {
             'label': `${label}`,
             'font-size': '12px',
-            "text-margin-y": "-5px",
-
           }
         }
     );
